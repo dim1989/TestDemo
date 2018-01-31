03f16cac01ea9ad400244627c2042f5b9f2756db
DimZhang <hzzhangzj@gmail.com>
2018-01-31 13:25:58 +0800

    Initial commit

---
+++ app/src/main/java/klm/myapplication/KLMButton.java
@@ -0,0 +1,26 @@
+package klm.myapplication;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+
+/**
+ * @author by dim
+ * @data 2018/1/31 13:25
+ * 邮箱：271756926@qq.com
+ */
+
+public class KLMButton extends AppCompatButton{
+
+    public KLMButton(Context context) {
+        super(context);
+    }
+
+    public KLMButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public KLMButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
